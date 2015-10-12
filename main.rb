8b729d7f9dfd3983a77a1ae3b9996d794c050ba3
aarongoa <taipan93@gmail.com>
2015-10-12 19:36:39 +0530

    Made code modular & Added required fields for donation & Other changes

--- main.rb
+++ main.rb
@@ -32,58 +32,40 @@ get '/' do
 end
 
 get '/test' do
-	status = send_mail("abc@google.com", "Description text", "10:30am")
+	status = send_mail("abc@google.com", "Description text", nil)
 	status
 end
 
-#Retrieve User Data
+# Retrieve User Data
 get '/:email' do
-	user_email = params[:email].to_s
-	
-	feeder = Feeders.first(:email => user_email)
+	user_data = get_user(params[:email].to_s)
 
-	if feeder.nil?
+	if user_data.nil?
 		return settings.does_not_exist
 	end
 
 	content_type :json
-	{
-		:email => feeder.email,
-		:name => feeder.name,
-		:phone => feeder.phone,
-		:state => feeder.state,
-		:address => feeder.address,
-		:times_fed => feeder.times_fed
-	}.to_json
+	user_data.to_json
 end
 
-#sign_in
+# Sign in
 post '/login/:email' do
-	login_email = params[:email].to_s
-	
-	feeder = Feeders.first(:email => login_email)
+	user_data = get_user(params[:email].to_s)
 
-	if feeder.nil?
+	if user_data.nil?
 		return settings.does_not_exist
 	end
 
 	content_type :json
-	{
-		:email => feeder.email,
-		:name => feeder.name,
-		:phone => feeder.phone,
-		:state => feeder.state,
-		:address => feeder.address,
-		:times_fed => feeder.times_fed
-	}.to_json
+	user_data.to_json
 end
 
-#sign_up
+# Sign up
 post '/feeders/:email/:name/:phone/:state/:address' do
 
-	feeder = Feeders.first(:email => params[:email].to_s)
+	user_data = get_user(params[:email].to_s)
 
-	if feeder.nil?
+	if user_data.nil?
 		feeder = Feeders.new
 
 		feeder.email = params[:email].to_s
@@ -110,24 +92,43 @@ post '/feeders/:email/:name/:phone/:state/:address' do
 end
 
 # Make donation
-
 post '/donation/:email/:description/:time' do
+
+	user_data = get_user(params[:email].to_s)
+
+	if user_data.nil?
+		return settings.does_not_exist
+	end
 
 	email = params[:email].to_s
 	description = params[:description].to_s
 	time = params[:time].to_s
 
+	# Additional parameters passed
 	url = params[:url]
+	location = params[:location]
+	foodtype = params[:foodtype]
+	packing = params[:packing]
+	foodfor = params[:foodfor]
 
 	if url.nil?
 		return settings.no_img
 	end
 
+	food_info = "Description: " + description + "Foodtype: " + foodtype + 
+		"Packaging: " + packing + "Food for: " + foodfor + 
+		"Preferable pickup time: " + time
+
+	if !location.nil?
+		food_info = food_info + "Location: " + location
+	end
+
+	# Downloads file to attach it later
 	File.open('tmp/image.png', 'wb') do |fo|
 		fo.write open(url).read 
 	end
 
-	status = send_mail(email, description, time)
+	status = send_mail(email, food_info, location)
 
 	return status
 end
@@ -145,7 +146,25 @@ end
 
 
 helpers do
-	def send_mail (email, description, time)
+
+	def get_user(email)
+		feeder = Feeders.first(:email => email)
+		if feeder.nil?
+			return nil
+		end
+		user_data = {
+			:email => feeder.email,
+			:name => feeder.name,
+			:phone => feeder.phone,
+			:state => feeder.state,
+			:address => feeder.address,
+			:pincode => feeder.pincode,
+			:times_fed => feeder.times_fed
+		}
+		return user_data
+	end
+
+	def send_mail (email, food_info, location)
 
 		feeder = Feeders.first(:email => email)
 
@@ -153,15 +172,20 @@ helpers do
 			return settings.does_not_exist
 		end
 
+		feeder.times_fed += 1
+		feeder.save
+
 		# Create message with donation data
 		donation_msg = "Email: " + email + "\nName: " + feeder.name + 
 			"\nPhone: " + feeder.phone + "\nState: " + feeder.state + 
-			"\nAddress: " + feeder.address + "\nDescription: " + 
-			description + "\nTime: " + time + 
-			"\nTimes Donated: " + feeder.times_fed.to_s
+			"\nPin-Code: " + feeder.pincode + "Times Donated:" + feeder.times_fed +
+			"\n" + food_info
 
-		feeder.times_fed += 1
-		
+			if location.nil?
+				donation_msg = donation_msg + "\nAddress: " + feeder.address
+			end
+
+
 		# Regular Mail
 		options = { :address				=> "smtp.gmail.com",
 					:port					=> 587,
@@ -177,9 +201,9 @@ helpers do
 		end
 
 		Mail.deliver do
-			to 'hhlc5@slipry.net'
+			to 'hhlda@slipry.net' #'aldrichm69@gmail.com'
 			from "donator@xyz.com"
-			subject 'Subject'
+			subject 'Food Donation'
 			body donation_msg
 			add_file "#{Dir.pwd}/tmp/image.png"
 		end
