0945cee9f274d3151e8b3944a927ace2bd6c33a8
aarongoa <taipan93@gmail.com>
2015-10-12 19:45:04 +0530

    Updated script to populate db

--- data_feeders.rb
+++ data_feeders.rb
@@ -1,14 +1,14 @@
-require './feeders'
+require './main'
 
 (10..20).each do |x|
 	feeder = Feeders.new
 	
 	feeder.email = x.to_s + '@gmail.com'
-	feeder.pass = 'password'
 	feeder.name = 'John Doe'
 	feeder.phone = rand(999) + 8000000000
 	feeder.state = 'The Shire, Middle Earth'
 	feeder.address = 'Bag End,end of Bagshot Row, Hobbiton'
+	feeder.pincode = '400100'
 	feeder.times_fed = rand(20)
 	
 	feeder.save
